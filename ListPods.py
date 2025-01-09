317fabda010148546280c4e06bfaaca8d0864657
thomas danan <thomasdanan@tda-scality.local>
2025-01-09 23:30:29 +0100

    print pod list on certain conditions

--- ListPods.py
+++ ListPods.py
@@ -7,12 +7,23 @@ class SosPods:
         self.pod_dirs = Path(sosarchive).glob('sos_commands/metalk8s/by-namespaces/*/pod')
 
     def list_pods(self):
-        print("=======================  PODS  =======================")
+        print("==============================  PODS  ==============================")
+        print("### Display pods showing restarts or not being in Running state  ###")
         for pod_dir in self.pod_dirs:
             pod_list_file = os.path.join(pod_dir, "list.txt")
             if os.path.exists(pod_list_file):
-                print(f"\nNamespace: {pod_dir.parent.basename()}")
+                print(f"Namespace: {pod_dir.parent.basename()}")
                 with open(pod_list_file, 'r') as file:
-                    print(file.read().strip())
+                    lines = file.readlines()
+                    for line in lines:
+                        trimmedLine = ' '.join(line.split())
+                        if(trimmedLine.startswith("NAME")):
+                            continue
+                        parts = trimmedLine.split(' ')
+                        if(parts[2] == "Completed"):
+                            continue
+                        #parts[3] = nbr of restarts, parts[2] is pod status parts[1] is nbr of container Running
+                        if(int(parts[3]) > 0 or (parts[2] != "Running") or (parts[1] != "1/1" and parts[1] != "2/2" and parts[1] != "3/3")):
+                            print(line.strip())
             else:
                 print(f"No pod list found in {pod_dir}")
--- SosTopo.py
+++ SosTopo.py
@@ -8,15 +8,6 @@ from path import Path
 from PromStat import PromStat, AGGR
 from MongoDataStore import MongoDataStore
 
-
-#hyperdrive_http_bytes_reclaimable_free_space.json:                    "__name__": "hyperdrive_http_bytes_reclaimable_free_space",
-#hyperdrive_relocation_byte_reclaimed_total.json:                    "__name__": "hyperdrive_relocation_byte_reclaimed_total",
-#hyperdrive_http_bytes_net.json
-#hyperdrive_http_key_net.json
-#hyperdrive_index_capacity.json
-# hyperdrive_index_free.json
-#hyperdrive_index_nbkeys.json
-
 class SosTopo:
 
 
@@ -28,12 +19,7 @@ class SosTopo:
         with open(sosnodes + "/list.txt", 'r') as file:
             next(file)
             for line in file:
-                self.nodes.append(line)
-        #files = os.listdir(self.sosnodes)
-        #for file in files:
-    #        if (re.search("(.*)eval_db.getCollection___infost", file)):
-    #                self.datastore.append(sosarchive +'/sos_commands/artesca/' + str(file))
-
+                self.nodes.append(line.strip())
 
     def extractTopo(self):
         print("=======================  TOPO  =======================")
@@ -43,10 +29,3 @@ class SosTopo:
     def extractNodeTopo(self):
         for node in self.nodes:
             print(node)
-
-    #def extractMongoTopo(self):
-    #    sosmongos= sosarchive +'/sos_commands/artecsa/'
-    #    files = os.listdir(sosmongos)
-    #    for mongostatuses in files:
-    #        with open(mongostatuses, 'r') as file:
-    #            jsonMongoStatuses = json.load(file)
